c712b8f97457c033d261e4592b5a9a6916c628d8
Ccolombe12 <ccolombe@g.hmc.edu>
2024-12-17 13:04:11 -0600

    added solutions

---
+++ 2024/AOC_helpers.py
@@ -0,0 +1,51 @@
+import re
+import pyrival as pr
+
+
+def scan_nums(line):
+    # Use a regular expression to find numbers
+    numbers = re.findall(r'-?\d+\.?\d*', line)
+    # Convert to appropriate numerical types
+    numbers = [float(num) if '.' in num else int(num) for num in numbers]
+    return numbers
+
+def read_input(filename):
+    with open(filename, 'r') as f:
+        data = f.read().splitlines()
+    return data
+
+def write_output(data, filename):
+    with open(filename, 'w') as f:
+        f.write(data)
+
+def print_board(board):
+    for row in board:
+        print(''.join(row))
+        
+def get_blocks(input):
+    """
+    Splits a list of strings into blocks separated by empty lines.
+    """
+    blocks = []
+    cur_block = []
+    for line in input:
+        if line == '\n':
+            blocks.append(cur_block)
+            cur_block = []
+        else:
+            cur_block.append(line)
+    blocks.append(cur_block)
+    return blocks
+
+def get_lines(input):
+    """
+    Splits a list of strings into lines.
+    """
+    return [line.strip() for line in input]
+
+def get_nums(input):
+    """
+    Splits a list of strings into numbers.
+    """
+    return [int(num) for num in input]
+
---
+++ 2024/problem 17/test.py
@@ -0,0 +1,4 @@
+#test.py
+
+# in this document we will test our code for problem 17
+# we will also test the codeium e
